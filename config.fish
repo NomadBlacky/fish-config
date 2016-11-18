7057be66e4b23d68b9fd8543a4e5aec8ce409de5
NomadBlacky <hourou.hamurinzu@gmail.com>
2016-11-18 22:40:17 +0900

    Fix git aliases.

--- config.fish
+++ config.fish
@@ -9,13 +9,13 @@ rbenv init - fish | source
 
 alias e="emacsclient ."
 set -x PATH $HOME/.cask/bin $PATH
-alias gits="git status"
 alias emacs='emacs -nw'
 set -x PATH $HOME/bin/activator-dist-1.3.10/bin $PATH
-alias gitcm="git commit -m"
-alias gitf="git fetch"
-alias gitpu="git push"
-alias gitd="git diff"
-alias gitadda="git add --all"
+alias gs="git status"
+alias gcm="git commit -m"
+alias gf="git fetch"
+alias gpu="git push"
+alias gd="git diff"
+alias gadda="git add --all"
 set -x PATH $HOME/.conscript/bin $PATH
 set -x EDITOR 'emacs -q -nw'
