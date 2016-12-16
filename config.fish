46a9e55ac8474fae61b53e8d61f21428a77e924c
NomadBlacky <hourou.hamurinzu@gmail.com>
2016-12-16 18:41:27 +0900

    Fix conscript variables

--- config.fish
+++ config.fish
@@ -9,7 +9,8 @@ rbenv init - fish | source
 
 set -x PATH $HOME/.cask/bin $PATH
 set -x PATH $HOME/bin/activator-dist-1.3.10/bin $PATH
-set -x PATH $HOME/.conscript/bin $PATH
+set -x CONSCRIPT_HOME $HOME/.conscript
+set -x PATH $CONSCRIPT_HOME/bin $PATH
 set -x EDITOR 'emacsclient -nw'
 set -x PATH $HOME/.evm/bin $PATH
 set -x GOPATH $HOME/.go
