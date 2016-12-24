f6913c0620da0d1ac00277af1e626e99537c0741
NomadBlacky <hourou.hamurinzu@gmail.com>
2016-12-25 00:56:59 +0900

    Fix 'cdr' function

--- functions/cdr.fish
+++ functions/cdr.fish
@@ -1,5 +1,8 @@
 function cdr
-	tail -30 $HOME/.config/fish/tmp/recent_dir.list | sort | uniq | peco --layout=bottom-up | read d
+	tail -100 $HOME/.config/fish/tmp/recent_dir.list | \
+	ruby -ne 'BEGIN{$list=[]}; $list << $_; END{puts $list.reverse.uniq}' | \
+	peco --layout=bottom-up | read d
+	
 	if [ $d ]
 	   cd $d
 	end
