aab5d3ac2f681e0e961c7c6f6a87023a58489957
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-01-06 21:18:03 +0900

    Format the code

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,18 +1,17 @@
 function fish_user_key_bindings
-	read -l theme < $OMF_CONFIG/theme
-  # Prepare packages key bindings paths
-  set -l key_bindings $OMF_CONFIG/key_binding?.fish \
-                      {$OMF_CONFIG,$OMF_PATH}/pkg/*/key_bindings.fish \
-                      {$OMF_CONFIG,$OMF_PATH}/themes/$theme/key_binding?.fish
-  # Source all keybindings collected
-  for file in $key_bindings
-    source $file
-  end
+    read -l theme <$OMF_CONFIG/theme
+    # Prepare packages key bindings paths
+    set -l key_bindings $OMF_CONFIG/key_binding?.fish {$OMF_CONFIG,$OMF_PATH}/pkg/*/key_bindings.fish {$OMF_CONFIG,$OMF_PATH}/themes/$theme/key_binding?.fish
+    # Source all keybindings collected
+    for file in $key_bindings
+        source $file
+    end
 
-  bind \cr peco_select_history
-  bind \eg git-insert-hash
+    bind \cr peco_select_history
+    bind \eg git-insert-hash
+    bind \eh backward-kill-word
 
-  # Call original key bindings if existent
-#  functions -q __original_fish_user_key_bindings
-#    and __original_fish_user_key_bindings
+    # Call original key bindings if existent
+    #  functions -q __original_fish_user_key_bindings
+    #    and __original_fish_user_key_bindings
 end
