70251588e24b8517335aa4ec1e986bf79aaa1701
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-01-31 11:57:37 +0900

    Fix to use w3m browser by `weblio` function

--- functions/weblio.fish
+++ functions/weblio.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 2
 function weblio
-	echo $argv | xargs -n1 | sed -E "s/^/https:\/\/ejje.weblio.jp\/content\//" | xargs -IURL xdg-open URL
+	echo $argv | xargs -n1 | sed -E "s/^/https:\/\/ejje.weblio.jp\/content\//" | xargs -IURL w3m URL
 end
