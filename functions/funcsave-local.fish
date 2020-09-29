265750797368004bb152c352864e69ec1a091750
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2020-09-29 10:47:37 +0900

    Fix funcsave-local

--- functions/funcsave-local.fish
+++ functions/funcsave-local.fish
@@ -1,5 +1,6 @@
 function funcsave-local
-	funcsave $argv[1]
-	 cd $HOME/.config/fish/functions/
-	 mv $argv[1].fish local/
+    set --local FUNCTIONS_DIR $HOME/.config/fish/functions/
+    mkdir -p $FUNCTIONS_DIR/local/
+    funcsave $argv[1]
+    mv $FUNCTIONS_DIR/$argv[1].fish $FUNCTIONS_DIR/local/
 end
